13a9a60612415e219d4637d50f9250069fa835d7
kdmin0706 <gxxk76@gmail.com>
2023-11-07 11:52:29 +0900

    ProductData 추가

---
+++ src/main/java/com/springboot/jpa/data/service/impl/ProductServiceImpl.java
@@ -0,0 +1,77 @@
+package com.springboot.jpa.data.service.impl;
+
+import com.springboot.jpa.data.dao.ProductDao;
+import com.springboot.jpa.data.dto.ProductDto;
+import com.springboot.jpa.data.dto.ProductResponseDto;
+import com.springboot.jpa.data.entity.Product;
+import com.springboot.jpa.data.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductDao productDao;
+
+    @Autowired
+    public ProductServiceImpl(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @Override
+    public ProductResponseDto getProduct(Long number) {
+        Product product = productDao.selectProduct(number);
+
+        ProductResponseDto productResponseDto = ProductResponseDto.builder()
+                .number(product.getNumber())
+                .name(product.getName())
+                .price(product.getPrice())
+                .stock(product.getStock())
+                .build();
+
+        return productResponseDto;
+    }
+
+    @Override
+    public ProductResponseDto saveProduct(ProductDto productDto) {
+        Product product = Product.builder()
+                .name(productDto.getName())
+                .price(productDto.getPrice())
+                .stock(productDto.getStock())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        Product savedProduct = this.productDao.insertProduct(product);
+
+        ProductResponseDto productResponseDto = ProductResponseDto.builder()
+                .number(savedProduct.getNumber())
+                .name(savedProduct.getName())
+                .price(savedProduct.getPrice())
+                .stock(savedProduct.getStock())
+                .build();
+
+        return productResponseDto;
+    }
+
+    @Override
+    public ProductResponseDto changeProductName(Long number, String name) throws Exception {
+        Product changedProduct = this.productDao.updateProductName(number, name);
+
+        ProductResponseDto productResponseDto = ProductResponseDto.builder()
+                .number(changedProduct.getNumber())
+                .name(changedProduct.getName())
+                .price(changedProduct.getPrice())
+                .stock(changedProduct.getStock())
+                .build();
+
+        return productResponseDto;
+    }
+
+    @Override
+    public void deleteProduct(Long number) throws Exception{
+        this.productDao.deleteProduct(number);
+    }
+}
